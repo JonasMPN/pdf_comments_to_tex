5c25e85591fac5968de55dae4b017f4404510e80
Jonas MPN <you@example.com>
2024-07-25 00:01:11 +0200

    section creation done

--- collected_to_tex.py
+++ collected_to_tex.py
@@ -1,41 +1,32 @@
-def directory_to_section(name: str, level_idx: int):
+import json
+import pylatex as tex
+
+
+def paper_notes_to_tex_paragraph(tex_document: tex.document, paper_data: dict):
     """
-    >> match level_idx:
-        >> case 0:
-            >> create section with name
-        >> case 1:
-            >> create subsection with name
-        >> case 2: 
-            >> create subsubsection with name
-        >> case 3: 
-            >> raise error
     """
 
 
-def paper_notes_to_tex_paragraph():
-    """
-    
-    """
-
-
-def collected_json_to_tex():
-    """
-    >> notes = load json (dict)
-    >> idx_to_section = {0: Section, 1: Subsection, 2: Subsubsection}
-    >> level_idx = 0
-    >> define recursive function loop_notes(dir_data, level_idx) that loops over child, child_data in dir_data.items()
-        >> for child, child_data in dir_data.items():
-            >> if not child.startswith("f_"):
-                This means the key is still a directory
-                >> create section/subsection/subsubsection with idx_to_section[level_idx]
-                >> directory_to_section(level_idx)
-                >> level_idx += 1
-                >> level_idx = loop_notes(child_data, level_idx)
-            >> else:
-                This means the key is now the name of a paper
-                >> paper_name = child[1:] (skip the "f_")
-                Use method paper_notes_to_tex_paragraph to create the paragraph
-                >> paper_notes_to_tex_paragraph(child_data)
+def collected_json_to_tex(ff_json: str, save_as: str="collected"):
+    idx_to_section = {0: tex.Section, 1: tex.Subsection, 2: tex.Subsubsection, 
+                      3: tex.section.Paragraph}
+    def loop_notes(tex_document: tex.Document, dir_data: dict, level_idx: int):
+        for child, child_data in dir_data.items():
+            if not child.startswith("f_"):
+                with tex_document.create(idx_to_section[level_idx](child)):
+                    level_idx += 1
+                    level_idx = loop_notes(tex_document, child_data, level_idx)
+            else:
+                with tex_document.create(idx_to_section[3](child[2:-4])):
+                    paper_notes_to_tex_paragraph(tex_document, child_data)
         return level_idx-1
-    """
     
+    doc = tex.Document(documentclass="article", document_options="a4paper")
+    doc.packages.append(tex.NoEscape(r'\usepackage{booktabs}'))
+    doc.packages.append(tex.NoEscape(r'\usepackage{hyperref}'))
+    
+    with open(ff_json, "r") as file_notes:
+        notes = json.load(file_notes)
+    level_idx = 0
+    loop_notes(doc, notes, level_idx)
+    doc.generate_tex(save_as)
