2a986dec9b28d052738888848bf409b7a63ac3d5
Jonas <jonas.mpn@gmail.com>
2024-08-14 10:22:53 +0200

    updated to current state

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from collect_from_pdfs import collect_notes
-from collected_to_tex import collected_json_to_tex
+from collected_to_tex import collected_notes_to_tex
 
 if __name__ == "__main__":
-    collect_notes(write_json=True)
-    collected_json_to_tex("collected_notes.json")
+    collected_notes = "collected_notes.json"
+    # collected_notes = collect_notes(write_json=collected_notes)
+    # collected_notes_to_tex(collect_notes)
+    collected_notes_to_tex(ff_json=collected_notes)
